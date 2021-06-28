6001c6a7ba3ede9bfe32cb92d263d8b2a097186f
YamadaKouki <215x022x@stu.kobe-u.ac.jp>
2021-06-28 15:15:01 +0900

    macに同期

--- todo/src/main/java/jp/kobespiral/yamadakouki/todo/controller/ToDoController.java
+++ todo/src/main/java/jp/kobespiral/yamadakouki/todo/controller/ToDoController.java
@@ -8,7 +8,36 @@ import org.springframework.stereotype.Controller;
 public class ToDoController {
     @Autowired
     ToDoService tService;
-    
+    /**
+     * ToDoサービスへのログイン HTTP-GET /member/login
+     * @param 
+     * @return
+     */
+    @GetMapping("/login")
+    String todoLogin(){
+        model.addAttribute();
+        return "login";
+    }
+    /**
+     * ユーザー用・ToDo確認
+     * @param form
+     * @param model
+     * @return
+     */
+    @GetMapping("/{mid}/todos")
+    Strign seeTodos(@PathVariable String user, Model model) {
 
+    }
+    /**
+    * ユーザ用・ToDoリストを表示 HTTP-POST /{mid}/todo/check
+    * @param form
+    * @param model
+    * @return
+    */
+   @PostMapping("/{mid}/todo/check") 
+   String checkUserToDo(@ModelAttribute(name = "ToDoForm") ToDoForm form,  Model model) {
+       model.addAttribute("ToDoForm", form);
+       return "check";
+   }
 
 }
